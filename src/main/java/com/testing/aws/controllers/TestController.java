79f9f939907bbeb463d91f6a4f706964d4c9d747
chanisagithub <sadin.chanisa8@gmail.com>
2024-06-05 13:30:07 +0530

    applcaition properties update

--- src/main/java/com/testing/aws/controllers/TestController.java
+++ src/main/java/com/testing/aws/controllers/TestController.java
@@ -3,27 +3,32 @@ package com.testing.aws.controllers;
 
 import com.testing.aws.dto.crudResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("test")
 @RequiredArgsConstructor
 public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     private final WebClient.Builder webClientBuilder;
 
     @PostMapping
     public Mono<String> addUser(@RequestBody crudResponses user) {
+
         return webClientBuilder.
                 build().
                 post().
                 uri("/api/test").
                 bodyValue(user).
                 retrieve().
-                bodyToMono(String.class);
+                bodyToMono(String.class)
+                .doOnSuccess(res -> logger.info("Response: {}", res))
+                .doOnError(error -> logger.error("Error: ", error));
     }
 
     @GetMapping
@@ -33,6 +38,8 @@ public class TestController {
                 get().
                 uri("/api/test").
                 retrieve().
-                bodyToMono(String.class);
+                bodyToMono(String.class)
+                .doOnSuccess(res -> logger.info("Response: {}", res))
+                .doOnError(error -> logger.error("Error: ", error));
     }
 }
